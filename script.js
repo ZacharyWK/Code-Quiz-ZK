522df2a77a8e4f618c826fd9ab496f5ec626b754
Zachary K <zachwkrause@gmail.com>
2021-06-07 13:38:36 -0700

    20210607 - Update
    update saves previous scores and displays all together

--- script.js
+++ script.js
@@ -1,177 +1,189 @@
-// const sb = document.querySelector("#startBtn");
-// const lab = document.querySelector("#inputLable");
-// const tex = document.querySelector("#inputText");
-// const ans = document.querySelector("#answers");
-// const tim = document.querySelector("#time");
-// const form = document.querySelector("#form");
-// const que = document.querySelector("#question");
-// const a = document.querySelector("#a");
-// const b = document.querySelector("#b");
-// const c = document.querySelector("#c");
-// const sco = document.querySelector("#score");
-// const pla = document.querySelector("#place");
+const sb = document.querySelector("#startBtn");
+const lab = document.querySelector("#inputLable");
+const tex = document.querySelector("#inputText");
+const ans = document.querySelector("#answers");
+const tim = document.querySelector("#time");
+const form = document.querySelector("#form");
+const que = document.querySelector("#question");
+const a = document.querySelector("#a");
+const b = document.querySelector("#b");
+const c = document.querySelector("#c");
+const sco = document.querySelector("#score");
+const pla = document.querySelector("#place");
 
-// const hid = document.querySelector(".hid");
-// const vis = document.querySelector(".vis");
-
-
-
-
-// let scoreLIST = [];
-// let savedNAME = null;
-// let savedPOINTS = null;
-// let points = 0;
+let scoreLIST = JSON.parse(localStorage.getItem("LIST")) || [];
+let savedNAME = null;
+let savedPOINTS = null;
+let points = 0;
 
 
 
-// let timer = tim.value;
-// let i = 0;
+let timer = tim.value;
+let i = 0;
 
-// const questions = [
-//     {
-//     question:"Question 1? answer is Yes.",
-//     right: "a",
-//     answers:{a:"yes",b:"no",c:"maybe"}
-//     },
-//     {
-//     question:"Question 2? answer is 2.",
-//     right: "b",
-//     answers:{a:"1",b:"2",c:"3" }
-//     },
-//     {
-//     question:"Question 3? answer is Best.",
-//     right: "c",
-//     answers:{a:"worst",b:"okayst",c:"best",}
-//     },
-//     {question:"END",
-//     answers:{a:"",b:"",c:"",}
-//     }
-// ];
+// QUESTIONS
+const questions = [
+    {
+    question:"Question 1? answer is Yes.",
+    right: "a",
+    answers:{a:"yes",b:"no",c:"maybe"}
+    },
+    {
+    question:"Question 2? answer is 2.",
+    right: "b",
+    answers:{a:"1",b:"2",c:"3" }
+    },
+    {
+    question:"Question 3? answer is Best.",
+    right: "c",
+    answers:{a:"worst",b:"okayst",c:"best",}
+    },
+    {question:"END",
+    answers:{a:"",b:"",c:"",}
+    }
+];
+// Start
+function startBtn() {
 
-// function startBtn() {
+    event.stopPropagation
 
-//     console.log('start');
+    console.log('start');
 
-//     sb.classList.add("hid");
-//     sb.classList.remove("vis");
+    sb.classList.add("hid");
+    sb.classList.remove("vis");
 
-//     lab.classList.add("vis");
-//     lab.classList.remove("hid");
+    lab.classList.add("vis");
+    lab.classList.remove("hid");
     
-//     tex.classList.add("vis");
-//     tex.classList.remove("hid");
+    tex.classList.add("vis");
+    tex.classList.remove("hid");
 
-// };
 
-// function enterName() 
-// {
-//     console.log(`Quiz Start`);
+};
+// NAME
+function enterName() 
+{
+    console.log(`Quiz Start`);
 
-//     lab.classList.add("hid");
-//     lab.classList.remove("vis");
+    lab.classList.add("hid");
+    lab.classList.remove("vis");
     
-//     tex.classList.add("hid");
-//     tex.classList.remove("vis");
+    tex.classList.add("hid");
+    tex.classList.remove("vis");
     
-//     que.classList.add("vis");
-//     que.classList.remove("hid");
+    que.classList.add("vis");
+    que.classList.remove("hid");
     
-//     ans.classList.add("vis");   
-//     ans.classList.remove("hid");
+    ans.classList.add("vis");   
+    ans.classList.remove("hid");
     
-//     tim.classList.remove("hid");
-//     tim.classList.add("vis");
+    tim.classList.remove("hid");
+    tim.classList.add("vis");
     
-//     loadIn();
-//     startTimer();
-// };
+    loadIn();
+    startTimer();
+};
 
-// function end() {
-//     EndPoints(); 
-//     score();
+// NEXT
+function next() {
+    i++;
+    loadIn();
+    if (i === 3) {end()};
+}
 
-//     que.classList.add("hid");
-//     que.classList.remove("vis");
+function loadIn() {
+    que.innerHTML = questions[i].question;
+    a.innerHTML = questions[i].answers.a;
+    b.innerHTML = questions[i].answers.b;
+    c.innerHTML = questions[i].answers.c;   
+};
+
+// END
+function end() {
+    clearInterval(tim.value);
+    EndPoints(); 
+    score();
+
+    que.classList.add("hid");
+    que.classList.remove("vis");
     
-//     ans.classList.add("hid");   
-//     ans.classList.remove("vis");
+    ans.classList.add("hid");   
+    ans.classList.remove("vis");
     
-//     tim.classList.add("hid");
-//     tim.classList.remove("vis");
+    tim.classList.add("hid");
+    tim.classList.remove("vis");
 
-//     sco.classList.remove("hid");
-//     sco.classList.add("vis");
+    sco.classList.remove("hid");
+    sco.classList.add("vis");
 
-//     pla.classList.add("vis");
+    pla.classList.add("vis");
     
-//     tim.textContent = 0;
-// }
-
-// function next() {
-//     i++;
-//     loadIn();
-//     if (i === 3) {end()};
-// }
-
-// function loadIn() {
-//     que.innerHTML = questions[i].question;
-//     a.innerHTML = questions[i].answers.a;
-//     b.innerHTML = questions[i].answers.b;
-//     c.innerHTML = questions[i].answers.c;   
-// };
+    // tim.textContent = 0;
+}
 
 
-// function startTimer() {
-//     tim.value = setInterval(function() {
-//         if (tim.textContent == 0){end()}
-//         else {tim.textContent--;}
-//         }, 1000)
+// TIMER
+function startTimer() {
+    tim.value = setInterval(function() {
+        if (tim.textContent == 0){
+            // clearInterval(tim.value), 
+            end()}
+        else {tim.textContent--;}
+        }, 1000)
     
-// }
+}
 
-// function wrong() {
-//     console.log("wrong answer");
-//     console.log(tim.textContent);
+// ANSWERS
+function wrong() {
+    console.log("wrong answer");
+    console.log(tim.textContent);
 
-//     tim.textContent = parseInt(tim.textContent, 10)-20;
-//     console.log(tim.textContent);
+    tim.textContent = parseInt(tim.textContent, 10)-20;
+    console.log(tim.textContent);
 
-//     next();
-// }
+    next();
+}
 
-// function EndPoints() {
-//     let timeScore = 0;
-//     if (tim.textContent > 0){timeScore+=25}
-//     let Epoints = (points+=timeScore);
-//     console.log(Epoints);
-//     savedPOINTS = localStorage.setItem("POINTS", JSON.stringify(Epoints));
-//     console.log(savedPOINTS);  
-// };
+//BUTTONS
+a.onclick = function(e) {
+    if (questions[i].right == "a"){points+=25, next()}
+    else {wrong()};
+};
+b.onclick = function(e) {
+    if (questions[i].right == "b"){points+=25, next()}
+    else {wrong()};
+};
+c.onclick = function(e) {
+    if (questions[i].right == "c"){points+=25, next()}
+    else {wrong()};
+};
 
-// a.onclick = function(e) {
-//     if (questions[i].right == "a"){points+=25, next()}
-//     else {wrong()};
-// };
+// SCORE
+function score() {
+   console.log("SCORE")
+    let endScore = localStorage.getItem("NAME") +" scored "+ localStorage.getItem("POINTS") +" "+ "Points";
+    scoreLIST.push(endScore)
+    localStorage.setItem("LIST", JSON.stringify(scoreLIST));
+    // scoreLIST = JSON.parse(localStorage.getItem("LIST")) ;   
+    pla.textContent = JSON.stringify(scoreLIST);
+};
 
-// b.onclick = function(e) {
-//     if (questions[i].right == "b"){points+=25, next()}
-//     else {wrong()};
-// };
-
-// c.onclick = function(e) {
-//     if (questions[i].right == "c"){points+=25, next()}
-//     else {wrong()};
-// };
-
-// function score() {
-//     pla.textContent = localStorage.getItem("NAME") +" scored "+ localStorage.getItem("POINTS") +" "+ "Points";
-// };
+function EndPoints() {
+    let timeScore = 0;
+    if (tim.textContent > 0){timeScore+=25}
+    let Epoints = (points+=timeScore);
+    console.log(Epoints);
+    savedPOINTS = localStorage.setItem("POINTS", JSON.stringify(Epoints));
+    console.log(savedPOINTS);  
+};
 
 
-// form.addEventListener("submit", function(e) {
-//     e.preventDefault();
-//     console.log(tex.value);
-//     name = tex.value;
-//     savedNAME = localStorage.setItem("NAME", name);
-//     enterName();
-// });
+
+// SUBMIT NAME
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    console.log(tex.value);
+    name = tex.value;
+    savedNAME = localStorage.setItem("NAME", name);
+    enterName();
+})
